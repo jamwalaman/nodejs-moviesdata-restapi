735072440ba29ed8b2001eda342fcbfd0e932b70
Aman Jamwal <aman.jamwal95@gmail.com>
2023-07-01 15:23:34 +1200

    Add host to app.listen function

--- app.js
+++ app.js
@@ -21,4 +21,4 @@ app.use('/api/movies', movies);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${port}`));
