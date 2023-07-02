9ebc2afa5eb8eca3cdb277fc62a64865ce6f7443
Aman Jamwal <aman.jamwal95@gmail.com>
2023-07-02 15:54:09 +1200

    Add default port

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) => res.send('Hello world!'));
 
 app.use('/api/movies', movies);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${port}`));
