d22966d2ac46305abfc4213bd8536e90aa217ee6
Aman Jamwal <aman.jamwal95@gmail.com>
2023-07-21 14:56:33 +1200

    Minor change. Code indentation

--- models/Movie.js
+++ models/Movie.js
@@ -1,27 +1,13 @@
 const mongoose = require('mongoose');
 
 const MovieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  director: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String
-  },
-  release_date: {
-    type: Date
-  },
-  production: {
-    type: String
-  },
-  updated_date: {
-    type: Date,
-    default: Date.now
-  }
+  title: {type: String, required: true},
+  director: {type: String, required: true},
+  synopsis: {type: String},
+  production: {type: String},
+  release_date: {type: Date},
+  added_on: {type: Date, default: Date.now},
+  updated_date: {type: Date, default: Date.now}
 });
 
 module.exports = Movie = mongoose.model('movie', MovieSchema);
